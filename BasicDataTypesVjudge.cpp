36726fc88deb54f886516989e2d7350278b1ff12
Shefa21 <152915092+Shefa21@users.noreply.github.com>
2024-07-27 01:44:06 +0600

    Create BasicDataTypesVjudge.cpp

---
+++ BasicDataTypesVjudge.cpp
@@ -0,0 +1,25 @@
+https://vjudge.net/problem/Gym-287306B
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+    long long ll;
+    char ch;
+    float f;
+    double d;
+    
+    // Read input values
+    cin >> i >> ll >> ch >> f >> d;
+    
+    // Print each value on a new line
+    cout << i << endl;
+    cout << ll << endl;
+    cout << ch << endl;
+    cout << f << endl;
+    cout << d << endl;
+
+    return 0;
+}
